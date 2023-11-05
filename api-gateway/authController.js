82dcbe8253e04376bc0de86b81f04de2b84e44d5
Alex <80361639+alex-cyberpunk@users.noreply.github.com>
2023-11-05 03:00:57 +0000

    mod 4 - aula 7 - 5 topico

--- api-gateway/authController.js
+++ api-gateway/authController.js
@@ -8,14 +8,29 @@ async function doLogin(req,res,next){
     && password === '123456'){
 
         const token = jwt.sign({userId : 1},
-            process.env.SECRET,{expiresIn:process.env.EXPIRES})//Passe poucas infos 
+            process.env.SECRET,{expiresIn:parseInt(process.env.EXPIRES)})//Passe poucas infos 
         res.json({token});    
     }
     else res.sendStatus(401);
 }
 
-async function doLogout(req,res,next){
+async function validateToken(req,res,next){
+    let token= req.headers['authorization'];
+    token = token.replace('Beader','');
+    try{
+        const {userId}=jwt.verify(token,process.env.SECRET)
+        res.locals.userId=userId;
+        next();
+    }
+    catch(error){
+        console.log(error)
+        res.sendStatus(401);
+    }
     
 }
 
-module.exports={doLogin,doLogout};
+async function doLogout(req,res,next){
+    res.send('Logout !')
+}
+
+module.exports={doLogin,doLogout,validateToken};
